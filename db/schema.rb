db9e13bca4e3e37f14bb96d5b9d872d8b755da16
wendy0402 <wendykurniawan92@gmail.com>
2015-11-29 15:14:00 +0700

    add column author to books

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129073828) do
+ActiveRecord::Schema.define(version: 20151129080834) do
 
   create_table "books", force: :cascade do |t|
     t.string  "title"
     t.integer "quantity"
     t.text    "summary"
     t.integer "price"
+    t.string  "author"
   end
 
 end
