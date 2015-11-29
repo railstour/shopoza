07e9ff3731c72bee1daa6bed18f683d1a41bba38
wendy0402 <wendykurniawan92@gmail.com>
2015-11-29 14:39:29 +0700

    create migration for table books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201133502) do
+ActiveRecord::Schema.define(version: 20151129073828) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                                null: false
@@ -33,4 +33,10 @@ ActiveRecord::Schema.define(version: 20151201133502) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
+  create_table "books", force: :cascade do |t|
+    t.string  "title"
+    t.integer "quantity"
+    t.text    "summary"
+    t.integer "price"
+  end
 end
