5b11057fe4edfc6afa992183438d2fb233b4bfeb
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-12-02 07:03:41 +0700

    re-track updated database schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129045902) do
+ActiveRecord::Schema.define(version: 20151201133502) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "name",                                null: false
+    t.text     "address"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -24,8 +28,6 @@ ActiveRecord::Schema.define(version: 20151129045902) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
