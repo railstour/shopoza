09b985696c16fde5b010cfd076eaa6b160889498
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-12-03 20:24:41 +0700

    devise use our own controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+  	confirmations: "users/confirmations",
+  	omniauth: "users/omniauth",
+  	passwords: "users/passwords",
+  	registrations: "users/registrations",
+  	sessions: "users/sessions",
+  	unlocks: "users/unlocks"
+  }
+  
   root to: "home#index"
 end
