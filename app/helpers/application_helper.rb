412f10ea96117d47816cc2d8e1149d7bf5122017
wendy0402 <wendykurniawan92@gmail.com>
2015-12-03 22:00:19 +0700

    set summary length to 100 only

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def money_amount(amount)
     number_to_currency(amount, unit: 'Rp.')
   end
+
+  def short_summary(words, length, link)
+    summary = words[0 .. length]
+    "#{summary} #{link_to('see more', link)}".html_safe
+  end
 end
