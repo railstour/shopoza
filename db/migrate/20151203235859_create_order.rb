4903d3ac1812080ff56755592f4c339a10caf701
wendy0402 <wendykurniawan92@gmail.com>
2015-12-04 07:02:23 +0700

    add order

---
+++ db/migrate/20151203235859_create_order.rb
@@ -0,0 +1,9 @@
+class CreateOrder < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.integer :gross_amount
+      t.references :user, index: true, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129080834) do
+ActiveRecord::Schema.define(version: 20151203235859) do
 
   create_table "books", force: :cascade do |t|
     t.string  "title"
@@ -20,5 +20,14 @@ ActiveRecord::Schema.define(version: 20151129080834) do
     t.integer "price"
     t.string  "author"
   end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "gross_amount"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id"
 
 end
