8341bd21052fce7683027d82c570260ed08bdbdd
wendy0402 <wendykurniawan92@gmail.com>
2015-12-05 07:09:19 +0700

    add column status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204133127) do
+ActiveRecord::Schema.define(version: 20151205000732) do
 
   create_table "books", force: :cascade do |t|
     t.string  "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20151204133127) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "status"
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id"
