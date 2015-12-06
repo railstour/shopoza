5deb31f856b05182701a162b2b0e22c8df1601af
wendy0402 <wendykurniawan92@gmail.com>
2015-12-06 08:54:14 +0700

    add total_price to orders(total_price is sum of item price)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151206002644) do
+ActiveRecord::Schema.define(version: 20151206015328) do
 
   create_table "books", force: :cascade do |t|
     t.string  "title"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20151206002644) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "status"
+    t.integer  "total_price"
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id"
