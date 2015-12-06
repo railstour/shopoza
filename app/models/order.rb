e5b691050410b156c7ea60ab125e47db7e5c11c8
wendy0402 <wendykurniawan92@gmail.com>
2015-12-06 20:42:03 +0700

    set price before save for order and order detail

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,10 @@ class Order < ActiveRecord::Base
   validates_inclusion_of :status, in: ['shopping']
 
   scope :for_user, lambda{ |user| where(user: user) }
+
+  before_save :set_total_price
+
+  def set_total_price
+    self.total_price = order_details.sum('unit_price * quantity')
+  end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,10 @@
 class OrderDetail < ActiveRecord::Base
   belongs_to :order
   belongs_to :book
+
+  before_save :set_unit_price
+
+  def set_unit_price
+    self.unit_price = book.price
+  end
 end
