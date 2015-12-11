69e8eb1da667cbba270542feed3300364d5abcb3
wendy0402 <wendykurniawan92@gmail.com>
2015-12-12 05:49:01 +0700

    update price order add more checking

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -4,6 +4,7 @@ class OrderDetail < ActiveRecord::Base
 
   before_save :set_unit_price
   after_save :update_price_orders
+  after_destroy :update_price_orders
 
   def set_unit_price
     self.unit_price = book.price
@@ -13,9 +14,10 @@ class OrderDetail < ActiveRecord::Base
     unit_price * quantity
   end
 
-
   def update_price_orders
-    order.set_total_price
-    order.save
+    if order.present?
+      order.set_total_price
+      order.save
+    end
   end
 end
