1187492db08d258c3635114386367caf118dec35
wendy0402 <wendykurniawan92@gmail.com>
2015-12-11 08:28:00 +0700

    create link for delete

--- app/controllers/order_details_controller.rb
+++ app/controllers/order_details_controller.rb
@@ -7,6 +7,14 @@ class OrderDetailsController < ApplicationController
     redirect_to :back
   end
 
+  def destroy
+    order_detail = OrderDetail.joins(:order).find_by(id: params[:id], orders: { user_id: current_user.id })
+    if order_detail.present?
+      order_detail.destroy
+    end
+    redirect_to order_path(params[:order_id])
+  end
+
   private
   def order_detail_params
     params.require(:order_detail).permit(:quantity)
