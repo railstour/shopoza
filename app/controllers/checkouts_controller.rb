50ec5c39a196a3f92020abf4e64cefbad9b34d97
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-12-19 13:36:44 +0700

    rename controller to `CheckoutsController`

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,12 +1,12 @@
-class CheckoutController < ApplicationController
+class CheckoutsController < ApplicationController
   before_action :authenticate_user!
 
   def create
   	order = Order.for_user(current_user).status_shopping
     if order.present?
-      redirect_to root_path, notice: "Nothing to checkout"
-    else
       redirect_to root_path, notice: "Checked out!"
+    else
+      redirect_to root_path, notice: "Nothing to checkout"
     end
   end
 
