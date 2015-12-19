ebbb34f0a8ecfce2a93e892bfc35a96f98e7c62f
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-12-19 17:29:38 +0700

    make `transaction_details` method on `Order`

--- app/models/order.rb
+++ app/models/order.rb
@@ -30,4 +30,24 @@ class Order < ActiveRecord::Base
   def set_total_price
     self.total_price = order_details.sum('unit_price * quantity')
   end
+
+  def transaction_details
+    {
+      transaction_details: {
+        order_id: id,
+        gross_amount: total_price
+      },
+      item_details: [].tap do |item_details|
+        order_details.each do |item_detail|
+          book = item_detail.book
+          item_details << {
+            id: book.id,
+            price: book.price,
+            quantity: item_detail.quantity,
+            name: book.title
+          }
+        end
+      end
+    }
+  end
 end
