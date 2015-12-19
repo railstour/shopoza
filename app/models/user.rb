f25945a4d318dd9f538f86feee16682d685fa5a9
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-12-19 17:48:46 +0700

    add `last_name` and `first_name` to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,12 @@ class User < ActiveRecord::Base
       name_part.capitalize
     end.join(" ")
   end
+
+  def first_name
+    name.split(" ").first
+  end
+
+  def last_name
+    (name.split(" ")[1..-1]).join(" ") || name
+  end
 end
