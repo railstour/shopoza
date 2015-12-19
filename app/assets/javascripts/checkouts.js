56a62cfc6f03c1afed7553d05e4d7843a3c01d27
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-12-19 22:26:02 +0700

    styling and js for the form

--- app/assets/javascripts/checkouts.js
+++ app/assets/javascripts/checkouts.js
@@ -3,8 +3,16 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).ready(function() {
-	$(".payment_options").on("click", "li", function() {
+	$(".payment_options").on("click", "li a", function() {
+		// set the current option to selected_option
 		$(".payment_options li").removeClass("selected_option")
 		$(this).addClass("selected_option")
+
+		// display the form
+      $(".payment_option_forms .payment_form").removeClass("selected_option")
+      form_class = ".payment_form" + "." + $(this).attr("id")
+		$(".payment_option_forms")
+         .find(form_class)
+         .addClass("selected_option")
 	})
 })
