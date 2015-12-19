464792ce3c24c0224de13e3552266871892a6837
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-12-19 13:31:03 +0700

    dummy `create` action on `Checkout`

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,5 +1,13 @@
 class CheckoutController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+  	order = Order.for_user(current_user).status_shopping
+    if order.present?
+      redirect_to root_path, notice: "Nothing to checkout"
+    else
+      redirect_to root_path, notice: "Checked out!"
+    end
   end
 
   def index
