3015939ab328f88fbdda4a1c336e6f6391be85b5
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-12-24 11:12:17 +0700

    checkout_id in migration

--- db/migrate/20151203235859_create_order.rb
+++ db/migrate/20151203235859_create_order.rb
@@ -2,6 +2,7 @@ class CreateOrder < ActiveRecord::Migration
   def change
     create_table :orders do |t|
       t.references :user, index: true, foreign_key: true
+      t.string :checkout_id, index: true
       t.timestamps
     end
   end
