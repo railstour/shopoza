5dfd09e61af6c3f06d017979f964f5023f1015b1
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-12-24 11:09:11 +0700

    code for checkout

--- app/assets/javascripts/checkouts.js
+++ app/assets/javascripts/checkouts.js
@@ -3,16 +3,88 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).ready(function() {
-	$(".payment_options").on("click", "li a", function() {
-		// set the current option to selected_option
-		$(".payment_options li").removeClass("selected_option")
-		$(this).addClass("selected_option")
+  if ($(".payment_options").length > 0) {
+    // Set Veritrans client key
+    Veritrans.client_key = window.veritransClientKey
+    Veritrans.url = "https://api.sandbox.veritrans.co.id/v2/token"
 
-		// display the form
+    // when clicking a payment option, display the form
+    $(".payment_options").on("click", "li a", function(event) {
+      event.preventDefault()
+
+      // set the current option to selected_option
+      $(".payment_options li").removeClass("selected_option")
+      $(this).addClass("selected_option")
+
+      // display the form
       $(".payment_option_forms .payment_form").removeClass("selected_option")
-      form_class = ".payment_form" + "." + $(this).attr("id")
-		$(".payment_option_forms")
-         .find(form_class)
-         .addClass("selected_option")
-	})
+      formClass = ".payment_form" + "." + $(this).attr("id")
+      $(".payment_option_forms")
+      .find(formClass)
+      .addClass("selected_option")
+    })
+
+    // == for mandiri only
+    $(".payment_option_forms .pay_manclick #card_number").on("keyup", function() {
+      ccNumber = $(this).val()
+      if (ccNumber.length > 9) {
+        $("#input1").val(ccNumber.substring(ccNumber.length, ccNumber.length - 10))
+      }
+    })
+
+    // generate random number
+    $("#input3").val(function() {
+      var pad = new Array(1 + 5).join(0)
+      return (pad + Math.floor(Math.random() * 99999)).slice(-pad.length)
+    }())
+    // == end for mandiri only
+
+    // == for credit card only
+    var card = function() {
+      return {
+        'card_number': $("#cc_card_number").val(),
+        'card_exp_month': $("#cc_card_exp_month").val(),
+        'card_exp_year': $("#cc_card_exp_year").val(),
+        'card_cvv': $("#cc_card_cvv").val()
+      }
+    }
+
+    $(".payment_option_forms .pay_cc input[type='submit']").on("click", 
+      function(event) {
+        event.preventDefault()
+        Veritrans.token(card, function callback(response) {
+          if (response.status_code == '200') {
+            $("#cc_token_id").val(response.token_id)
+            $.ajax({
+              type: "POST",
+              url: $(".payment_option_forms .pay_cc").attr("action"),
+              data: {
+                'payment_type': 'credit_card',
+                'credit_card': {
+                  'token_id': $("#cc_token_id").val()
+                }
+              },
+              beforeSend: function() {
+                window.openSpinner()
+              },
+              complete: function(e) {
+                window.closeSpinner()
+                response = e.responseJSON
+                if (response.success == 1 || response.success == '1') {
+                  window.location = "/"
+                } else {
+                  alert("There is an error in checking out. Please contact us Admin")
+                }
+              },
+            })
+            $(".payment_option_form .pay_cc form").submit()
+          } else {
+            alert(response.status_message)
+            console.log(JSON.stringify(response))
+          }
+        })
+    })
+    $("#cc_card_number").makeCreditCardField()
+    // == end for credit card only
+  }
 })
--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,12 +1,18 @@
 class CheckoutsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_shopping_cart
+  before_action :ensure_checkoutable, except: [:vt_payment_callbacks]
+  before_action :define_base_url
 
-  USE_VT_WEB = true
+  VeritransResult = Struct.new :redirect_url, :status_message, :status_code
+  STAGING = :staging
+  PRODUCTION = :production
+  ACCOUNT_TYPE = STAGING # or production
+  USE_VT_WEB = false # false
 
   def create
     if @order.present?
-      USE_VT_WEB ? checkout_web : checkout_direct
+      USE_VT_WEB ? checkout_web : redirect_to(direct_checkouts_path)
     else
       redirect_to root_path, notice: "Nothing to checkout"
     end
@@ -22,10 +28,93 @@ class CheckoutsController < ApplicationController
   def direct_form
   end
 
+  def vt_payment_callbacks
+    checkout_id = params[:order_id]
+    status_code = params[:status_code]
+    transaction_status = params[:transaction_status]
+    order = Order.for_user(current_user).where(checkout_id: checkout_id).first
+
+    if order && status_code.to_s == "200" &&
+      transaction_status.to_s.downcase == "capture"
+      order.update_attributes(status: Order::STATUS_PAYMENT_RECEIVED)
+      redirect_to root_path,
+        notice: "Your payment have been received. We will process it shortly"
+    else
+      redirect_to root_path, alert: "Sorry, could not process the payment"
+    end
+  end
+
+  def checkout_direct
+    charging_params = params.merge(@order.transaction_details)
+    @result = charge(charging_params)
+
+    status_code = @result.status_code.to_s
+
+    if status_code == '200' # successful transaction
+      @order.update_attributes(status: Order::STATUS_PAYMENT_RECEIVED)
+      flash[:notice] = "Your payment have been received. We will process it shortly"
+      render json: {success: 1}
+    elsif status_code == '202' # rejected by FDS/bank
+      render json: {success: 0}
+    elsif status_code == '201' # need manual confirmation at MAP
+      @order.update_attributes(status: Order::STATUS_WAITING_PAYMENT)
+      render json: {success: 1}
+    else
+      fail "Error: #{@result.status_message}"
+    end
+  rescue => e
+    puts e.message
+    render json: {success: 0}
+  end
+
   private
 
   def find_shopping_cart
     @order = Order.for_user(current_user).status_shopping.first
+  end
+
+  def ensure_checkoutable
+    redirect_to root_path, 
+      alert: "Nothing to checkout" if @order.nil?
+  end
+
+  def define_base_url
+    @base_url = if ACCOUNT_TYPE == STAGING 
+      "https://api.sandbox.veritrans.co.id/v2"
+    elsif ACCOUNT_TYPE == PRODUCTION
+      "https://api.veritrans.co.id/v2"
+    else
+      fail "Base url is undefined for unknown environment"
+    end
+  end
+
+  def charge(charging_params)
+    server_key = ACCOUNT_TYPE == :staging ? 
+      ENV["VT_DEV_SERVER_KEY"] : ENV["VT_PROD_SERVER_KEY"] 
+
+    vt_result = VeritransResult.new
+
+    RestClient::Request.execute(
+      method: :post,
+      url: "#{@base_url}/charge",
+      timeout: 40,
+      open_timeout: 40,
+      headers: {
+        "Authorization" => "Basic #{Base64.strict_encode64(server_key)}",
+        "Accept" => "application/json",
+        "Content-Type" => "application/json",
+        "User-Agent" => "Bukuniku"
+      },
+      payload: JSON.generate(charging_params)
+    ) do |response, request, result|
+      result = JSON.parse(response).try(:with_indifferent_access)
+
+      vt_result.redirect_url = result[:redirect_url]
+      vt_result.status_message = result[:status_message]
+      vt_result.status_code = result[:status_code]
+    end
+
+    vt_result
   end
 
   def checkout_web
@@ -33,17 +122,15 @@ class CheckoutsController < ApplicationController
       payment_type: 'VTWEB'
     }.merge(@order.transaction_details)
 
-    @result = Veritrans.charge(charging_params)
-    puts @result
+    # @result = Veritrans.charge(charging_params)
+    @result = charge(charging_params)
 
     if @result.redirect_url
+      @order.update_attributes(status: Order::STATUS_WAITING_PAYMENT)
       redirect_to @result.redirect_url
       return
     else
       redirect_to root_path, alert: @result.status_message
     end
-  end
-
-  def checkout_direct
   end
 end
