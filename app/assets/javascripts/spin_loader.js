52e0e887cd993783d2c1d31d28992b851e756f74
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-12-24 11:06:05 +0700

    create loader for the entire page

---
+++ app/assets/javascripts/spin_loader.js
@@ -0,0 +1,9 @@
+window.openSpinner = function() {
+  $("#spinnerFade").css("display", "block")
+  $("#spinnerModal").css("display", "block")
+}
+
+window.closeSpinner = function() {
+  $("#spinnerFade").css("display", "none")
+  $("#spinnerModal").css("display", "none")
+}
